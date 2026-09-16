5bcaff928ef4d4adf132c6a98beeeabcf7435963
kr0zapps <vidal.jonathans75@gmail.com>
2026-09-16 12:37:46 -0300

    fix(web): configure metro to resolve zustand CJS to eliminate import.meta syntax error on web

---
+++ metro.config.js
@@ -0,0 +1,20 @@
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+const defaultResolveRequest = config.resolver.resolveRequest;
+
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (platform === 'web' && (moduleName === 'zustand' || moduleName.startsWith('zustand/'))) {
+    return {
+      filePath: require.resolve(moduleName),
+      type: 'sourceFile',
+    };
+  }
+  if (defaultResolveRequest) {
+    return defaultResolveRequest(context, moduleName, platform);
+  }
+  return context.resolveRequest(context, moduleName, platform);
+};
+
+module.exports = config;
